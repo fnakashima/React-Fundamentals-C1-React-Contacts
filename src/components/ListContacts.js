099e158bb0991f99c152e30c4e5ca4b40ec62e3d
Fumihiro Nakashima <fuminaka387@gmail.com>
2024-10-05 18:12:54 +0100

    Completed 3.13

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,26 +1,63 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const ListContacts =  ({contacts, onDeleteContact}) => {
-    console.log("ListContacts: contacts: ", contacts);
-    console.log("ListContacts: onDeleteContact: ", onDeleteContact);
-    return <ol className="contact-list">
-        {
-            contacts.map(contact => (
-                <li key={contact.id} className="contact-list-item">
-                    <div 
-                        className="contact-avatar" 
-                        style={{backgroundImage: `url(${contact.avatarURL})`}}>
+    //console.log("ListContacts: contacts: ", contacts);
+    //console.log("ListContacts: onDeleteContact: ", onDeleteContact);
 
+    const [query, setQuery] = useState("");
+    const updateQuery = (query) => {
+        setQuery(query.trim());
+    }
+    const clearQuery =() => {
+        updateQuery("");
+    }
+    const showingContacts = query === ""
+      ? contacts
+      : contacts.filter(c => c.name.toLowerCase().includes(query.toLowerCase()));
+
+    return (
+        <div className="list-contacts">
+            <div className="list-contacts-top">
+                <input className="search-contacts" 
+                        type="text" 
+                        placeholder="Search Contacts"
+                        value={query}
+                        onChange={(event) => updateQuery(event.target.value)}>
+                </input>
+            </div>
+
+            {
+                showingContacts.length !== contacts.length && (
+                    <div className="showing-contacts">
+                        <span>
+                            Now showing {showingContacts.length} of {contacts.length}
+                        </span>
+                        <button onClick={clearQuery}>Show all</button>
                     </div>
-                    <div className="contact-details">
-                        <p>{contact.name}</p>
-                        <p>{contact.handle}</p>
-                    </div>
-                    <button className="contact-remove" onClick={() => onDeleteContact(contact)}>Remove</button>
-                </li>
-            ))
-        }
-    </ol>;
+                )
+            }
+
+            <ol className="contact-list">
+                {
+                    showingContacts.map(contact => (
+                        <li key={contact.id} className="contact-list-item">
+                            <div 
+                                className="contact-avatar" 
+                                style={{backgroundImage: `url(${contact.avatarURL})`}}>
+
+                            </div>
+                            <div className="contact-details">
+                                <p>{contact.name}</p>
+                                <p>{contact.handle}</p>
+                            </div>
+                            <button className="contact-remove" onClick={() => onDeleteContact(contact)}>Remove</button>
+                        </li>
+                    ))
+                }
+            </ol>            
+        </div>
+    )
 }
 
 ListContacts.propTypes = {
